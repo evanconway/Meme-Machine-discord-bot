7f36df99b9b1bc79010776ef78c387241eadb52d
AceOfHeart5 <evanconwaymusic@gmail.com>
2020-12-20 22:34:05 -0500

    add upvote command

--- bot.js
+++ bot.js
@@ -12,6 +12,10 @@ require('dotenv').config();
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const messages_max = 5;
+const messages = []; // collection of messages
+
+const BOTID = 
 
 client.on('ready', () => {
     console.log('Meme Machine is ready to go!');
@@ -19,43 +23,70 @@ client.on('ready', () => {
 
 client.on('message', msg => {
 
-    let msgArr = msg.content.split(' ');
-    let lowerArr = msg.content.toLowerCase().split(' ');
+    // add message to collection, but only if user is not the bot
+    if (msg.member.user.tag != 'Meme Machine#9639') {
 
-    /// Triple word effects
-    if (lowerArr.length === 3 && lowerArr[0] === lowerArr[1] && lowerArr[1] === lowerArr[2]) {
-        console.log('Triple word detected.');
+        // log message for effects
+        messages.push(msg);
+        if (messages.length > 5) messages.splice(0, 1);
+    
+        // delete message flag
+        let deleteMsg = false;
 
-        let pingEveryone = false;
-        if (lowerArr[0] === 'boys') pingEveryone = true;
-        if (lowerArr[0] === 'bois') pingEveryone = true;
-        if (lowerArr[0] === 'boyz') pingEveryone = true;
-        if (lowerArr[0] === 'guys') pingEveryone = true;
-        if (lowerArr[0] === 'guyz') pingEveryone = true;
+        let lowerArr = msg.content.toLowerCase().split(' ');
 
-        if (pingEveryone) {
-            msg.channel.send('@everyone ' + msg.member.user.username + ' has summoned the boys!');
+        // single word effects
+        if (lowerArr.length === 1 && lowerArr[0] === '^' && messages.length > 1) {
+            let prevMsg = messages[messages.length - 2];
+            prevMsg.react('⬆️');
+            deleteMsg = true;
         }
-    }
 
-    /*
-    if (msg.content === 'mod me') {
-        msg.member.roles.add('790370436675928094'); // mod id
-        msg.reply(`You've been modded!`);
-    }
+        // triple word effects
+        if (lowerArr.length === 3 && lowerArr[0] === lowerArr[1] && lowerArr[1] === lowerArr[2]) {
+            console.log('Triple word detected.');
 
-    if (msg.content === 'unmod me') {
-        msg.member.roles.remove('790370436675928094');
-        msg.reply(`You've been unmodded.`);
-    }
-    */
+            let pingEveryone = false;
+            if (lowerArr[0] === 'boys') pingEveryone = true;
+            if (lowerArr[0] === 'bois') pingEveryone = true;
+            if (lowerArr[0] === 'boyz') pingEveryone = true;
+            if (lowerArr[0] === 'guys') pingEveryone = true;
+            if (lowerArr[0] === 'guyz') pingEveryone = true;
 
-    if (msg.content === 'I love bots.') {
-        msg.react('❤️');
-    }
+            if (pingEveryone) {
+                msg.channel.send('@everyone ' + msg.member.user.username + ' has summoned the boys!');
+            }
+        }
 
-    if (msg.content === 'I hate bots.') {
-        msg.react('💩');
+        /*
+        if (msg.content === 'mod me') {
+            msg.member.roles.add('790370436675928094'); // mod id
+            msg.reply(`You've been modded!`);
+        }
+
+        if (msg.content === 'unmod me') {
+            msg.member.roles.remove('790370436675928094');
+            msg.reply(`You've been unmodded.`);
+        }
+        */
+
+        if (msg.content === 'I love bots.') {
+            msg.react('❤️');
+        }
+
+        if (msg.content === 'I hate bots.') {
+            msg.react('💩');
+        }
+
+        if (lowerArr[0] === 'ping') {
+            msg.channel.send('pong');
+        }
+
+        // handle delete message flag
+        if (deleteMsg) {
+            msg.delete();
+            messages.pop();
+        }
     }
 })
 
