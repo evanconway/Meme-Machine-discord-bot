911b58c9c3374e74ed3829b8fb1a4f3956e3fffc
AceOfHeart5 <evanconwaymusic@gmail.com>
2023-10-09 19:09:54 -0400

    add response model

--- meme_machine/discord_bot/models.py
+++ meme_machine/discord_bot/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Response(models.Model):
+    call = models.CharField(max_length=1000)
+    response = models.CharField(max_length=1000)
+
