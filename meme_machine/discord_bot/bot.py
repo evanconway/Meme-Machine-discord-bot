dd57a3eafa5622745595921d38005673160a96e4
AceOfHeart5 <evanconwaymusic@gmail.com>
2023-10-10 19:58:02 -0400

    add call response feature

--- meme_machine/discord_bot/bot.py
+++ meme_machine/discord_bot/bot.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import re
 from dotenv import load_dotenv
 from asgiref.sync import sync_to_async
 from discord_bot.models import Response
@@ -26,18 +27,34 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+    
+    check = str(message.content.lower())
 
-    response = await get_response(message.content.lower())
+    stored_response = await get_response(message.content.lower())
 
-    if response is not None:
-        print(response.response)
-        await message.channel.send(response.response)
+    if stored_response is not None:
+        await message.channel.send(stored_response.response)
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
+    if check.startswith('$set'):
+        split = re.split('[\[\]]', message.content)
+        if len(split) != 5:
+            await message.channel.send(f'Incorrect syntax. Format of $set command is: $set [call] [response]')
+            return
+        await Response(call=split[1].lower(), response=split[3]).asave()
+        await message.channel.send(f'Got it. When someone says "{split[1]}" I\'ll say "{split[3]}".')
+    
+    if check.startswith('$del'):
+        split = re.split('[\[\]]', message.content)
+        if len(split) != 3:
+            await message.channel.send(f'Incorrect syntax. Format of $del command is: $del [call]')
+            return
+        r = await get_response(split[1].lower())
+        if r is None:
+            await message.channel.send(f'I\'m not programmed to respond to "{split[1]}".')
+        else:
+            await r.adelete()
+            await message.channel.send(f'Got it. I won\'t respond to "{split[1]}" anymore.')
 
 def start_bot():
-    print('logged responses: ', Response.objects.all().count())
     client.run(os.getenv('BOT_TOKEN'))
 
